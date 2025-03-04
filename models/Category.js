1cce55f900b29cab1c2c7494699698ff4c3f5ba6
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-04 14:00:33 +0300

    category schema

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,7 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
-  }, 
+  },  
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
