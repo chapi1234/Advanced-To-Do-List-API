5c6527ccb2f9b31ffd0275c801f055ab406d8502
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-05 12:23:25 +0300

    category route

---
+++ routes/category.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const categoryController = require('../controllers/categoryController.js');
+
+// Create a new category
+router.post('/create', categoryController.createCategory);
+
+// Get all categories
+router.get('/', categoryController.getAllCategories);
+
+// Get a category by ID
+router.get('/:id', categoryController.getCategoryById);
+
+// Update a category
+router.put('/:id', categoryController.updateCategory);
+
+// Delete a category
+router.delete('/:id', categoryController.deleteCategory);
+
+module.exports = router;
