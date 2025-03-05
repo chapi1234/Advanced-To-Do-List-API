a8f1d3ab115655825af448c40fab40ddcc03135a
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-05 12:18:37 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 // Load config  
 dotenv.config({ path: './config/config.env' });
@@ -9,9 +12,25 @@ const MONGODB = process.env.MONGODB
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//  Importing routes
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/task');
+const categoryRoutes = require('./routes/category');
+const userRoutes = require('./routes/user');
+const reminderRoutes = require('./routes/reminder');
 
 // middleware 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+//  Route middleware
+app.use('/api/user', authRoutes);
+app.use('/api/task', taskRoutes)
+app.use('/api/category', categoryRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/reminder', reminderRoutes);
 
 // Connect to MongoDB
 mongoose.connect(MONGODB)
--- validation/validation.js
+++ validation/validation.js
@@ -19,7 +19,32 @@ const loginValidation = (data) => {
     return schema.validate(data, { abortEarly: false });
 };
 
+// Create task validation
+const createTaskValidation = (data) => {
+    const schema = Joi.object({
+        title: Joi.string().min(3).required(),
+        description: Joi.string().min(5).required(),
+        status: Joi.string().valid('pending', 'completed').default('pending'),
+        dueDate: Joi.date().optional(),
+        userId: Joi.string().required(),
+        categoryId: Joi.string()
+        // .required()
+    });
+    return schema.validate(data);
+};
+
+// Create category validation
+const createCategoryValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        user: Joi.string().required()
+    });
+    return schema.validate(data);
+};
+
 module.exports = {
     registerValidation,
-    loginValidation
+    loginValidation,
+    createTaskValidation,
+    createCategoryValidation
 };
